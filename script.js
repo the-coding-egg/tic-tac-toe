573ea468659005cb7d57c14e4f8f37450ebdf9ad
Dalton McMullen <thecodingegg@gmail.com>
2025-10-22 21:02:35 -0700

    Created Gameboard
    Build the gameboard while using a nested for loop to create a 2d array.

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("Hello world");
+function Gameboard() {
+  const row = 3;
+  const column = 3;
+  const board = [];
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let j = 0; j < column; j++) {
+      board[i].push(j);
+    }
+  }
+  return board;
+}
 
+console.log(Gameboard());
+
+function playerOne() {}
+
+function playerTwo() {}
